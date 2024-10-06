809a5d4990695c0740ae94f312e147350ffb3893
Emertat <Emertat@RAITY>
2024-10-06 14:34:55 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace resistor_color;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Color List: " + string.Join(", ", ResistorColor.Colors()));
+
+        Console.WriteLine("Enter the color:");
+        string? inputColor = Console.ReadLine()?.Trim().ToLower();
+
+        int colorCode = ResistorColor.ColorCode(inputColor);
+
+        if (colorCode != -1)
+        {
+            Console.WriteLine($"Color code for '{inputColor}' is: {colorCode}");
+        }
+        else
+        {
+            Console.WriteLine($"Invalid color '{inputColor}' entered. Please enter a valid color from the list.");
+        }
+    }
+}
---
+++ ResistorColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resistor_color;
+internal class ResistorColor
+{
+    public static string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int ColorCode(string color)
+    {
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colors[i] == color) { return i; }
+
+        }
+        return -1;
+    }
+
+    public static string[] Colors()
+    {
+        return colors;
+    }
+}
